dfaac63a9d45a05a43d6bb468f5a9d6ee2d96c72
Oliver Appel <oliver.appel@maritzcx.com>
2018-01-28 16:03:12 +0000

    Add objective model

---
+++ database/migrations/2018_01_28_155355_create_objectives_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateObjectivesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('objectives', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('employee_id');
+            $table->text('objective');
+            $table->string('evaluation');
+            $table->text('comment');
+            $table->boolean('mcx_core_values');
+            $table->boolean('personal_development');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('objectives');
+    }
+}
