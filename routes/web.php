7e82ca6003e1c1e4e9d90acbd7388e1ac0bfa649
Oliver Appel <oliver.appel@maritzcx.com>
2018-01-29 13:16:34 +0100

    Add route for objective list

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,12 @@ Route::post('/objectives/create', 'ObjectiveController@create')->name('objective
 
 Route::post('/objectives/{employee}/store', 'ObjectiveController@store')->name('objectives.store');
 
-Route::get('/objectives/{employee}/{objective}/show', 'ObjectiveController@show')->name('objectives.show');
+Route::get('/objectives/{objective}/show', 'ObjectiveController@show')->name('objectives.show');
 
 Route::get('/objectives/{employee}/{objective}/edit', 'ObjectiveController@edit')->name('objectives.edit');
 
 Route::post('/objectives/{employee}/{objective}/update', 'ObjectiveController@update')->name('objectives.update');
+
+Route::get('/objectives/{employee}/index', 'ObjectiveController@index')->name('objectives.index');
 
 Route::get('/employees/get', 'EmployeeController@get')->name('employees.get');
