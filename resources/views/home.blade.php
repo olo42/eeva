f92c238e03dd9c847e8fe35f0a5eefbe3718d78a
Oliver Appel <oliver.appel@maritzcx.com>
2018-01-29 12:35:39 +0100

    Add autocomplete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
-
+@section('styles')
+<style>
+.dropdown-menu {
+	position:absolut;
+	width:100%;
+	top: 40px !important;
+    left: 0px !important;
+}
+</style>
+@endsection
 @section('content')
 <div class="container">
     <div class="row">
@@ -14,7 +23,10 @@
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+	                                     
+
+                                <input id="email" type="email" class="form-control typeahead twitter-typeahead" 
+                                data-provide="typeahead" name="email" value="{{ old('email') }}" required autofocus autocomplete="off">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -28,7 +40,7 @@
                             <label for="name" class="col-md-4 control-label">Name</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required  autocomplete="off">
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -39,8 +51,8 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <a href="#" class="btn btn-default">
+                            <div class="col-md-6 col-md-offset-5">
+                                <a href="#" class="btn btn-default" style="visibility: hidden">
                                     Show older objectives
                                 </a>
                                 <button type="submit" class="btn btn-primary">
@@ -54,4 +66,39 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('scriptIncludes')
+<script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.10.2.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.js"></script>
+@endsection
+
+@section('scripts')
+var $input = $(".typeahead");
+	$input.typeahead({
+        minLength: 2,
+        delay: 400,
+        source: function (query, process) {
+            $.ajax({
+                url: '/employees/get/',
+                    data: {email: query},
+                    dataType: 'json'
+                })
+                .done(function(response) {
+                    $("#name").val('');
+                    $.each (response, function() {
+                        this.origin_name  = this.name;
+                        this.name = this.email;
+                    });
+                    console.log(response);
+                    return process(response);
+                });
+        },
+	    autoSelect: true,
+        afterSelect: function(item){
+            console.log(item);
+            $("#name").val(item.origin_name);
+        }
+	});
 @endsection
