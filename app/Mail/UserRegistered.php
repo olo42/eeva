5f18f91f2efc05281f5290aa46159e8fcbb0d66d
Oliver Appel <oliver.appel@maritzcx.com>
2018-01-31 09:57:58 +0100

    Send mail on user registration

---
+++ app/Mail/UserRegistered.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+use App\User;
+
+class UserRegistered extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $user;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mail.user_registered')->with([
+            'userName' => $this->user->name,
+            'userEmail' => $this->user->email])
+        ->subject('PX: New user registered');
+    }
+}
---
+++ resources/views/mail/user_registered.blade.php
@@ -0,0 +1,9 @@
+Hi,
+
+<p>The following new user has been added to the Employee Evaluation Application:</p>
+
+<p>Name: {{ $userName }}</p>
+
+<p>Email: {{ $userEmail }}</p>
+
+Thanks!
